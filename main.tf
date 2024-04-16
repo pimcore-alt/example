e22aaf0bb2181354c19f368f87752e19f796b11b
pimcore-alt <58470213+pimcore-alt@users.noreply.github.com>
2024-04-16 18:47:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
 provider "aws" {
-
-  # access_key = "${var.aws_access_key}"
-
-   # secret_key = "${var.aws_secret_key}"
-
     region = "sa-east-1"
-
 }
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+#  tags = {
+ #   Name        = "My bucket"
+  #  Environment = "Dev"
+#  }
 }
